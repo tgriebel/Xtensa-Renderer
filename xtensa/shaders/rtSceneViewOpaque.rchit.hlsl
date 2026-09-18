684acb7cc039b652891197af603d4bd257243ec9
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2026-09-18 18:26:47 -0500

    Refactor RT shaders so each method has its own file

--- xtensa/shaders/rtSceneViewOpaque.rchit.hlsl
+++ xtensa/shaders/rtSceneViewOpaque.rchit.hlsl
@@ -10,40 +10,7 @@ LIGHT_LAYOUT( 1, 5 )
 RT_PUSH_CONSTANTS
 
 #include "lighting.h"
-
-
-// Interpolate a float3 attribute across a triangle using barycentric weights.
-float3 BaryLerp3( float3 a, float3 b, float3 c, float b0, float b1, float b2 )
-{
-    return ( a * b0 + b * b1 + c * b2 );
-}
-
-float2 BaryLerp2( float2 a, float2 b, float2 c, float b0, float b1, float b2 )
-{
-    return ( a * b0 + b * b1 + c * b2 );
-}
-
-
-// Reconstructs surface sample at the intersection location
-sampleAttributes_t BuildSampleAttributes( const rtVertex_t v0, const rtVertex_t v1, const rtVertex_t v2, const float b0, const float b1, const float b2 )
-{
-    const float3 localNormal    = normalize( BaryLerp3( v0.normal,    v1.normal,    v2.normal,    b0, b1, b2 ) );
-    const float3 localTangent   = normalize( BaryLerp3( v0.tangent,   v1.tangent,   v2.tangent,   b0, b1, b2 ) );
-    const float3 localBitangent = normalize( BaryLerp3( v0.bitangent, v1.bitangent, v2.bitangent, b0, b1, b2 ) );
-
-    sampleAttributes_t surfaceSample;
-
-    surfaceSample.N = normalize( mul( localNormal, (float3x3)WorldToObject3x4() ) );
-    surfaceSample.T = normalize( mul( localTangent, (float3x3)ObjectToWorld3x4() ) );
-    surfaceSample.B = normalize( mul( localBitangent, (float3x3)ObjectToWorld3x4() ) );
-
-    surfaceSample.uv0 = BaryLerp2( v0.uv.xy, v1.uv.xy, v2.uv.xy, b0, b1, b2 );
-    surfaceSample.uv1 = BaryLerp2( v0.uv.zw, v1.uv.zw, v2.uv.zw, b0, b1, b2 );
-
-    surfaceSample.worldPosition = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
-
-    return surfaceSample;
-}
+#include "rtUtil.h"
 
 
 [shader( "closesthit" )]
